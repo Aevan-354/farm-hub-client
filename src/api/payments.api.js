726d915a02024d1889f6edd33405e632d8858cb9
=meltus-bwire <meltusbwire10@gmail.com>
2025-04-02 08:51:40 +0300

    chore: push the payment

--- src/api/payments.api.js
+++ src/api/payments.api.js
@@ -1,7 +1,8 @@
 import { API } from ".";
+import { getCurrentUser } from './current-user'
 
 export const iniatePayment = async ({land_id, amount, phoneNumber}) => {
-    const {data} =await API.post(`/payments/mpesa`, {land_id, amount, phone_number:formatPhoneNumber(phoneNumber)});
+    const {data} =await API.post(`/payments/mpesa`, {land_id, amount, user_id: getCurrentUser().id, phone_number:formatPhoneNumber(phoneNumber)});
     return data;
 }
 
